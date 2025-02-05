b96bda526ab6f4f26782c1009a2554844ca12137
sangeetharajamoorthy <sangeetha.rajamoorthy@straive.com>
2025-02-05 17:23:47 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"]) # Allow GET requests from all origins
+# Or, provide more granular control:
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://example.com"],  # Allow a specific domain
+    allow_credentials=True,  # Allow cookies
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Allow specific methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 def read_root():
